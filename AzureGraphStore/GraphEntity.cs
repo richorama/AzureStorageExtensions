167f3f857db6810e4793f1b7d6e95bb4cacdb905
Richard Astbury <Richard.Astbury@gmail.com>
2013-04-27 22:32:45 +0100

    initial commit

---
+++ AzureGraphStore/GraphEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Two10.AzureGraphStore
+{
+    class GraphEntity : TableEntity
+    {
+
+        public GraphEntity()
+        {
+        }
+
+        public GraphEntity(Triple triple)
+        {
+            this.Subject = triple.Subject;
+            this.Property = triple.Property;
+            this.Value = triple.Value;
+        }
+
+        public string Subject { get; set; }
+        public string Property { get; set; }
+        public string Value { get; set; }
+
+        public Triple ToTriple()
+        {
+            return new Triple(this.Subject, this.Property, this.Value);
+        }
+
+    }
+}
---
+++ AzureGraphStore/Triple.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Two10.AzureGraphStore
+{
+    public class Triple
+    {
+        public Triple(string subject, string property, string value)
+        {
+            this.Subject = subject;
+            this.Property = property;
+            this.Value = value;
+        }
+
+        public string Subject { get; set; }
+        public string Property { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}'s {2}", this.Subject, this.Property, this.Value);
+        }
+    }
+}
