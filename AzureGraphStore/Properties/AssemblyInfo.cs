fbba777abbdb1d06d083f5ece7986f40e7678414
Richard Astbury <Richard.Astbury@gmail.com>
2013-05-07 13:13:28 +0100

    added nuget

--- AzureGraphStore/Properties/AssemblyInfo.cs
+++ AzureGraphStore/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AzureGraphStore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Two10Degrees")]
 [assembly: AssemblyProduct("AzureGraphStore")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
